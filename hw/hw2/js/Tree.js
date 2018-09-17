95cccc818620604dc81eb235021b6d056ff89116
Alan <gahendersonjr@gmail.com>
2018-09-16 19:00:44 -0600

    assignPosition

--- hw/hw2/js/Tree.js
+++ hw/hw2/js/Tree.js
@@ -7,48 +7,63 @@ class Tree {
 	 */
 
 	constructor(json) {
-		this.nodes = [];
-		var nodeMap = new Map();
+		this.nodesList = [];
+		this.nodeMap = new Map();
 
 		for(var i in json){
 			var node = new Node(json[i].name, json[i].parent)
-			this.nodes.push(node);
-			nodeMap.set(node.name, node);
+			this.nodesList.push(node);
+			this.nodeMap.set(node.name, node);
 		}
 
-		for(var i in this.nodes){
-			var node = this.nodes[i];
-			node.parentNode = nodeMap.get(node.parentName);
+		for(var i in this.nodesList){
+			var node = this.nodesList[i];
+			node.parentNode = this.nodeMap.get(node.parentName);
 			if(node.parentNode){
 				node.parentNode.addChild(node);
 			}
 		}
-
-		// for(i in this.nodes){
-		// 	console.log(this.nodes[i]);
-		// }
 	}
 
 	/**
 	 * Function that builds a tree from a list of nodes with parent refs
 	 */
 	buildTree() {
+			this.deepestLevel = 0;
+			this.assignLevel(this.nodesList[0], 0);
+			this.positionMap = new Map();
+			this.assignPosition(this.nodesList[0], 0);
 
-        //Assign Positions and Levels by making calls to assignPosition() and assignLevel()
+			for(var i in this.nodesList){
+				console.log(this.nodesList[i]);
+			}
     }
 
 	/**
 	 * Recursive function that assign positions to each node
 	 */
 	assignPosition(node, position) {
-
+		if(node.parentNode && position<node.parentNode.position){
+			position = node.parentNode.position;
+		}
+		while(this.positionMap.get(`${node.level},${position}`)){
+			position++;
+		}
+		node.position = position;
+		this.positionMap.set(`${node.level},${position}`, true);
+		for(var i in node.children){
+			this.assignPosition(node.children[i], position+Number(i));
+		}
 	}
 
 	/**
 	 * Recursive function that assign levels to each node
 	 */
 	assignLevel(node, level) {
-
+		node.level = level;
+		for(var i in node.children){
+			this.assignLevel(node.children[i], level+1);
+		}
 	}
 
 	/**
