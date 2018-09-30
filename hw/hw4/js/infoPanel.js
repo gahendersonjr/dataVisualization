ee1effb88fa1313ba0df456be46b2dc209ec38cf
Alan <gahendersonjr@gmail.com>
2018-09-30 16:47:40 -0600

    removing comment shit

--- hw/hw4/js/infoPanel.js
+++ hw/hw4/js/infoPanel.js
@@ -24,16 +24,6 @@ class InfoPanel {
       item.innerText = oneWorldCup.teams_names[i];
       list.appendChild(item);
     }
-    // ******* TODO: PART III *******
-
-    // Update the text elements in the infoBox to reflect:
-    // World Cup Title, host, winner, runner_up, and all participating teams that year
-
-    // Hint: For the list of teams, you can create an list element for each team.
-    // Hint: Select the appropriate ids to update the text content.
-
-    //Set Labels
-
   }
 
 }
